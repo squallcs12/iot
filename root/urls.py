7eb30f2696dc4e109b3986422ee6e9cb07c7e154
Bang Dao <bang@inspectorio.com>
2018-10-02 23:28:41 +0700

    finish test data

--- root/urls.py
+++ root/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def data_view(request):
+    return HttpResponse('1')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data', data_view),
 ]
